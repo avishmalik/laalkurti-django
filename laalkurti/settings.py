977cb26347e50353df534395d2192d61c5b35bd8
Avish Malik <69065857+avishmalik@users.noreply.github.com>
2023-04-08 18:33:45 +0530

    fixed dj

--- laalkurti/settings.py
+++ laalkurti/settings.py
@@ -15,7 +15,9 @@ from decouple import config
 import dj_database_url
 import os
 import environ
+import mimetypes
 
+mimetypes.add_type("text/javascript", ".js", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
